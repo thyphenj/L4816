937076a0a5272a23185661076e949b5aa0ba8043
t-j <t-j@terence-john.co.uk>
2024-05-23 16:35:02 +0100

    Done - check for the answer in TheTimes

--- Clue.cs
+++ Clue.cs
@@ -4,9 +4,9 @@ using System.Diagnostics.Metrics;
 namespace L4816
 {
     public class Clue
-	{
+    {
         public string ClueName { get; set; }
-		public int ClueLength { get; set; }
+        public int ClueLength { get; set; }
         public Func<long, long> Formula1 { get; set; }
         public Func<long, long, long> Formula2 { get; set; }
 
@@ -16,12 +16,14 @@ namespace L4816
             ClueLength = cluelength;
             Formula1 = formula;
         }
-        public Clue(string clue, int cluelength, Func<long, long,long> formula)
+        public Clue(string clue, int cluelength, Func<long, long, long> formula)
         {
             ClueName = clue;
             ClueLength = cluelength;
             Formula2 = formula;
         }
+
+        // -------------------------------------------------------------------------------------
+
     }
 }
-
---
+++ Results.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace L4816
+{
+    static public class Results
+    {
+        static public void Display()
+        {
+            Dictionary<char, char> map = new Dictionary<char, char>()
+            {
+                {'1','o' },
+                {'2','t' },
+                {'3','t' },
+                {'4','f' },
+                {'5','f' },
+                {'6','s' },
+                {'7','s' },
+                {'8','e' },
+                {'9','n' }
+            };
+
+            string digits
+                = "61987316"
+                + "61192198"
+                + "87238962"
+                + "62889789"
+                + "28563838"
+                + "19836387"
+                + "98989857"
+                + "72678968";
+
+            int charno = 0;
+
+            Console.WriteLine("---------------------------------");
+            foreach (char ch in digits)
+            {
+                Console.Write(map[ch]);
+                if ( ++charno % 8 == 0 )
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
